9f3b7db6d60ecd421633c41055d56de801985c03
roidrole <roidorle@gmail.com>
2025-05-02 14:16:47 -0400

    Fix config when adding twice, removing first and adding another one

--- src/main/java/roidrole/patternbanners/config/Config.java
+++ src/main/java/roidrole/patternbanners/config/Config.java
@@ -53,7 +53,9 @@ public class Config {
         for (BannerPattern pattern : BannerPattern.values()) {genMappingFor(pattern);}
     }
     public static void genMappingFor(BannerPattern pattern){
-        patternAmount += 1;
+        while(mappingCategory.containsKey(String.valueOf(patternAmount))){
+            patternAmount += 1;
+        }
         ConfigCategory temp;
         if(pattern.hasPatternItem()){
             temp = new ConfigCategory(String.valueOf(patternAmount), mappingCategory);
