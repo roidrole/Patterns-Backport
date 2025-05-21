b2c5ccbe958a771056ffe0b475c5675e2b9edb3c
roidrole <roidorle@gmail.com>
2025-05-21 12:49:32 -0400

    Implement scrollable thumb

--- src/main/java/roidrole/patternbanners/loom/GuiLoom.java
+++ src/main/java/roidrole/patternbanners/loom/GuiLoom.java
@@ -5,45 +5,35 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.tileentity.BannerPattern;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.input.Mouse;
 import roidrole.patternbanners.Tags;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static roidrole.patternbanners.recipe._Recipe.PATTERNS_ONLY_SHAPE;
 
 @SideOnly(Side.CLIENT)
 public class GuiLoom extends GuiContainer {
-    private static final int SLOT_SIZE = 14;
-    public static List<ResourceLocation> patternLocs = new ArrayList<>();
+    public static final List<ResourceLocation> patternLocs = PATTERNS_ONLY_SHAPE.stream().map(
+    pattern -> new ResourceLocation("textures/entity/banner/"+pattern.getFileName()+".png")).collect(Collectors.toList());
+    public static final boolean hasScroll = patternLocs.size() > 16;
+
+    static int maxRenderedLine;
+
     private int firstRenderedLine = 0;
-    private static boolean hasScroll;
-    int scrollAmount;
-    int scrollPixels = 41;
     public GuiLoom(InventoryPlayer inventory, World world, BlockPos pos) {
         super(new ContainerLoom(inventory, world, pos));
-        this.height=176;
-        this.width=166;
-
-        for (BannerPattern pattern : PATTERNS_ONLY_SHAPE){
-            patternLocs.add(new ResourceLocation("minecraft", "textures/entity/banner/"+pattern.getFileName()+".png"));
-        }
-        hasScroll = patternLocs.size() > 16 || true;
-        if(hasScroll) {
-            scrollAmount = patternLocs.size() / 4 - 4;
+        if(hasScroll){
+            maxRenderedLine = Math.floorDiv(patternLocs.size(), 4) - 4;
         }
     }
-
-    /**
-     * Draws the screen and all the components in it.
-     */
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
@@ -51,14 +41,6 @@ public class GuiLoom extends GuiContainer {
         super.drawScreen(mouseX, mouseY, partialTicks);
         this.renderHoveredToolTip(mouseX, mouseY);
 
-        if(hasScroll){
-            Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("textures/gui/container/creative_inventory/tabs.png"));
-            drawTexturedModalRect(
-                    guiLeft + 12 - 6, guiTop + SLOT_SIZE * firstRenderedLine,
-                    232, 242, // Thumb texture position
-                    6, 15
-            );
-        }
         //Render the items
     }
 
@@ -75,20 +57,39 @@ public class GuiLoom extends GuiContainer {
         int i = this.guiLeft;
         int j = (this.height - this.ySize) / 2;
         this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
+
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("textures/gui/container/creative_inventory/tabs.png"));
+        this.drawTexturedModalRect(this.guiLeft+119, this.guiTop+13+getThumbOffset(firstRenderedLine), 232 +(hasScroll?0:12 ), 0, 12, 15);
     }
 
     //TODO : Finish implementing banner preview. FutureMC adds a listener to craftingMatrix. Vanilla output preview? Custom slot galore?
     //Just send an int representing the PATTERN_ONLY_RECIPES index of the pattern and calc on the server
     //Should check if there are enough PATTERN_ONLY_RECIPES to justify the bar. Vanilla is
-    @Override
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-    }
+    //TODO:Add click and drag slot
 
     @Override
     protected void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick) {
         super.mouseClickMove(mouseX, mouseY, clickedMouseButton, timeSinceLastClick);
     }
 
+    @Override
+    public void handleMouseInput() throws IOException {
+        super.handleMouseInput();
+        if (Mouse.getEventDWheel() == 0) {return;}
+        if(Mouse.getEventDWheel() > 0){
+            if(this.firstRenderedLine <= 0){return;}
+            firstRenderedLine--;
+        } else {
+            if(this.firstRenderedLine >= maxRenderedLine){return;}
+            firstRenderedLine++;
+        }
+    }
+
     //TODO : implement slots. Draw all slots at the same time to minimize isItemInSlot
+    //Helpers
+    public static int getThumbOffset(int first){
+        if(first == maxRenderedLine){return 41;}
+        return first*Math.floorDiv(41, maxRenderedLine);
+    }
 }
