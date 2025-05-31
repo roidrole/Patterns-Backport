dd4370bb62a23605fe294d12ce4a1488722cb984
roidrole <roidorle@gmail.com>
2025-05-31 10:56:52 -0400

    Optimize JEI category
    Draw a black rectangle instead of binding a black texture and drawing a textured rectangle

--- src/main/java/roidrole/patternbanners/recipe/wrapper/PatternOnlyShapeWrapper.java
+++ src/main/java/roidrole/patternbanners/recipe/wrapper/PatternOnlyShapeWrapper.java
@@ -45,7 +45,7 @@ public class PatternOnlyShapeWrapper extends PatternRecipeWrapper {
         super.drawInfo(mc, recipeWidth, recipeHeight, mouseX, mouseY);
         if(!ConfigGeneral.Recipes.craftingTable) {
             mc.getTextureManager().bindTexture(new ResourceLocation(Tags.MOD_ID, "textures/gui/container/slot/banner_pattern.png"));
-            drawTexturedModalRect(17, 31, 0, 0, 0, 16, 16, 0.0625f, 0.0625f);
+            drawTexturedModalRect(17, 31, 0, 0, 16, 16, 0.0625f, 0.0625f);
         }
     }
 }
--- src/main/java/roidrole/patternbanners/recipe/wrapper/PatternRecipeWrapper.java
+++ src/main/java/roidrole/patternbanners/recipe/wrapper/PatternRecipeWrapper.java
@@ -2,33 +2,44 @@ package roidrole.patternbanners.recipe.wrapper;
 
 import mezz.jei.api.recipe.IRecipeWrapper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
-import roidrole.patternbanners.Tags;
 
 public abstract class PatternRecipeWrapper implements IRecipeWrapper {
     String patternN;
 
     @Override
     public void drawInfo(Minecraft mc, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
-        mc.getTextureManager().bindTexture(new ResourceLocation(Tags.MOD_ID, "textures/gui/empty_banner.png"));
-        drawTexturedModalRect(82, 0, 0, 2, 2, 39, 78, 0.008f, 0.008f);
+        drawRect(82, 0, 121, 78, 0, 0, 0, 255);
         mc.getTextureManager().bindTexture(new ResourceLocation("minecraft", "textures/entity/banner/"+patternN+".png"));
-        drawTexturedModalRect(82, 0, 0, 2, 2, 39, 78, 0.008f, 0.008f);
+        drawTexturedModalRect(82, 0, 2, 2, 39, 78, 0.008f, 0.008f);
     }
 
-    // Helpers
-    // Took from Just Enough Pattern Banners, licensed GNU Lesser General Public License v3.0.
-    // Repo link : https://github.com/Lorexe/JustEnoughPatternBanners
-    public void drawTexturedModalRect(int x, int y, float z, int u, int v, int w, int h, float f, float f1) {
+    public static void drawRect(int left, int top, int right, int bottom, int R, int G, int B, int A) {
         Tessellator tessellator = Tessellator.getInstance();
-        tessellator.getBuffer().begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-        tessellator.getBuffer().pos(x, y + h, z).tex(u * f, (v + h) * f1).endVertex();
-        tessellator.getBuffer().pos(x + w, y + h, z).tex((u + w) * f, (v + h) * f1).endVertex();
-        tessellator.getBuffer().pos(x + w, y, z).tex((u + w) * f, v * f1).endVertex();
-        tessellator.getBuffer().pos(x, y, z).tex(u * f, v * f1).endVertex();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        GlStateManager.color(R, G, B, A);
+        bufferbuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
+        bufferbuilder.pos(left, bottom, 0.0D).endVertex();
+        bufferbuilder.pos(right, bottom, 0.0D).endVertex();
+        bufferbuilder.pos(right, top, 0.0D).endVertex();
+        bufferbuilder.pos(left, top, 0.0D).endVertex();
+        tessellator.draw();
+        GlStateManager.color(255, 255, 255, 255);
+    }
+
+    public static void drawTexturedModalRect(int x, int y, int u, int v, int w, int h, float f, float f1) {
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        bufferbuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+        bufferbuilder.pos(x, y + h, 0.0D).tex(u * f, (v + h) * f1).endVertex();
+        bufferbuilder.pos(x + w, y + h, 0.0D).tex((u + w) * f, (v + h) * f1).endVertex();
+        bufferbuilder.pos(x + w, y, 0.0D).tex((u + w) * f, v * f1).endVertex();
+        bufferbuilder.pos(x, y, 0.0D).tex(u * f, v * f1).endVertex();
         tessellator.draw();
     }
 }
